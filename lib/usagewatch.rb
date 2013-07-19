a043961f607281e118112c9d74ee02ef42138c15
Ruben Espinosa <rderoldan1@gmail.com>
2013-07-18 23:17:45 -0500

    osx validation

--- lib/usagewatch.rb
+++ lib/usagewatch.rb
@@ -3,10 +3,17 @@
 require "usagewatch/version"
 
 os = RUBY_PLATFORM
+text =  "OS is not supported in this version."
 
-if os.include?("darwin")
-  #require "usagewatch/usagewatch"
-  require File.join(File.dirname(__FILE__), 'usagewatch', 'usagewatch')
+if os.include? "darwin"
+  require "usagewatch/usagewatch"
+elsif os.include? "linux"
+  require "usagewatch/usagewatch"
+elsif os =~ /cygwin|mswin|mingw|bccwin|wince|emx/
+  puts "Windows" + text
 else
-  puts "This OS is not supported in this version."
+   puts "This" + text
 end
+
+
+
