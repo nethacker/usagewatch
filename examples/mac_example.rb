dc791b2a78b84d2a376cf7378a5b12018ce88f9b
Ruben Espinosa <rderoldan1@gmail.com>
2013-07-21 18:18:03 -0500

    mac example

--- examples/mac_example.rb
+++ examples/mac_example.rb
@@ -7,7 +7,8 @@ require 'usagewatch'
 usw =  Usagewatch
 
 puts "#{usw.uw_diskused} Gigabytes Used"
-puts "#{usw.uw_diskused_perc} Gigabytes Used"
+puts "#{usw.uw_diskused_perc} Percentage of Gigabytes Used"
+puts "#{usw.uw_memused}% Active Memory Used"
 puts "#{usw.uw_cpuused}% CPU Used"
 puts "#{usw.uw_load} Average System Load Of The Past Minute"
 puts "Top Ten Processes By CPU Consumption: #{usw.uw_cputop}"
